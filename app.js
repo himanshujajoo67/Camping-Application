8eb6a94dfeaa4081267f298bd0bf86741e8b36b2
Himanshu <hsj6@njit.edu>
2018-03-07 16:57:26 +0000

    Fix image loading problem

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ app.get("/",function(req, res) {
 
 app.get("/campground", function(req, res) {
    var campground = [
-       {name: "Salmon Creek", image: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjXobOSztjZAhURON8KHaowAXwQjRx6BAgAEAY&url=http%3A%2F%2Fweknowyourdreams.com%2Fcamping.html&psig=AOvVaw1-uyH3nrculCSZJPwb9ku2&ust=1520457073594135"},
-       {name: "Granite Hill", image: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjQmZ2fztjZAhVxkeAKHWRgDz8QjRx6BAgAEAY&url=https%3A%2F%2Fwww.travelok.com%2FCamping&psig=AOvVaw1-uyH3nrculCSZJPwb9ku2&ust=1520457073594135"},
-       {name: "Mountain Goat's Rest", image: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj-xsmlztjZAhUCneAKHQ20Ds4QjRx6BAgAEAY&url=http%3A%2F%2Fwww.nationalparks.nsw.gov.au%2Fcamping-and-accommodation%2Fsydney-and-surrounds-campgrounds&psig=AOvVaw1-uyH3nrculCSZJPwb9ku2&ust=1520457073594135"}
+       {name: "Salmon Creek", image: "https://www.photosforclass.com/download/pixabay-2271828"},
+       {name: "Granite Hill", image: "https://www.photosforclass.com/download/pixabay-2445212"},
+       {name: "Mountain Goat's Rest", image: "https://www.photosforclass.com/download/pixabay-2411069"}
        ] 
        res.render("campground", {campground:campground});
 });
