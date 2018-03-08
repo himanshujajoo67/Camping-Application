b082f6d9685f34a086c4d763dfb9d00bf88349f9
Himanshu <hsj6@njit.edu>
2018-03-08 07:50:02 +0000

    Add basic description for campgrounds and review RESTful Routes

--- app.js
+++ app.js
@@ -11,24 +11,26 @@ app.set("view engine", "ejs")
 // Scheme setup
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
-// Campground.create(
-//     {
-//       name: "Granite Hill", 
-//       image: "https://www.photosforclass.com/download/pixabay-2445212"
+Campground.remove(
+    {
+      name: "Dark Night River", 
+    //   image: "https://www.photosforclass.com/download/pixabay-2411069",
+    //   description: "This is a huge Granite hills, No Bathrooms, No water, Beautiful Granite Hills. Awesome Place to be All Alone on your Own"
         
-//     }, function(err, campground){
-//         if(err){
-//             console.log(err)
-//         } else {
-//             console.log("New Campground Created");
-//             console.log(campground);
-//         }
-//     })
+    }, function(err, campground){
+        if(err){
+            console.log(err)
+        } else {
+            console.log("New Campground Created");
+            console.log(campground);
+        }
+    })
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -36,8 +38,23 @@ app.get("/",function(req, res) {
     res.render("landing");  
 });
 
+
 app.get("/campground/new", function(req, res){
-   res.render("new.ejs"); 
+  res.render("new.ejs");
+
+});
+
+
+app.get("/campground/:id", function(req, res){
+    // find the campgroud with provided ID and render the show template for that campground
+    Campground.findById(req.params.id, function(err, foundCampground){
+       if(err){
+           console.log(err);
+       } else {
+            res.render("shows",{campground: foundCampground});
+       }
+    });
+  
 });
 
 app.get("/campground", function(req, res) {
@@ -46,7 +63,7 @@ app.get("/campground", function(req, res) {
         if(err){
             console.log(err)
         } else {
-               res.render("campground", {campground:allCampground});
+               res.render("index", {campground:allCampground});
         }
     })
    
@@ -56,11 +73,12 @@ app.post("/campground", function(req, res){
    //get data from forms and add to campground array
    var name = req.body.name;
    var image = req.body.image;
-   var newCampground ={name: name, image: image}
+   var desc = req.body.description;
+   var newCampground ={name: name, image: image, description: desc}
    //Create a new Campground and save to DB
    Campground.create(newCampground, function(err,newlyCreateCampground) {
        if(err){
-           console.lo(err);
+           console.log(err);
        } else {
             //redirect to campground
           res.redirect("/campground"); 
